815a46a932849ce703f560fbb8b921f3297aa937
aissa-laribi <aissalaribi@yahoo.fr>
2022-03-15 18:59:43 +0000

    Adding mysql

---
+++ mysql/mysql-server.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+sudo docker volume create mysql-volume
+sudo docker run -dit --name mysql alpine
+
+sudo docker exec -it mysql /bin/sh -c "apk update && apk --no-cache add mysql mysql->
+ && mkdir /var/run/mysqld \
+ && chown mysql:mysql /var/run/mysqld/ \
+ && mkdir /run/openrc \
+ && touch /run/openrc/softlevel \
+ && apk --no-cache add openrc \
+ && /etc/init.d/mariadb setup \
+ && rc-update add mariadb default \
+ && rc-status default \
+ && rc-service mariadb start \
+ && mysql"
