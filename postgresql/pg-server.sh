4e5e5a2f34a8d753bd618ac3fa01b6a7ad360628
aissa-laribi <aissalaribi@yahoo.fr>
2022-03-15 18:35:19 +0000

    Auto-login feature

--- postgresql/pg-server.sh
+++ postgresql/pg-server.sh
@@ -1,14 +1,14 @@
-#!bin/bash
+sudo docker volume create pg-server-volume
 sudo docker run -dit --name postgres alpine
 
-
-sudo docker exec -it postgres /bin/sh -c "apk update && apk --no-cache add postgresql \
-  && mkdir /run/postgresql/ \
-  && chown postgres:postgres /run/postgresql/"
-
-
-
-
-
-
-
+sudo docker exec -it postgres /bin/sh -c "apk update && apk --no-cache add postgresq>
+ && mkdir /run/postgresql/ \
+ && chown postgres:postgres /run/postgresql/ \
+ && mkdir /run/openrc \
+ && touch /run/openrc/softlevel \
+ && apk --no-cache add openrc \
+ && /etc/init.d/postgresql setup \
+ && rc-update add postgresql default \
+ && rc-status default \
+ && rc-service postgresql start \
+ && psql -U postgres"
